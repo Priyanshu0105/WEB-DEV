c43dc8cb71399cb9ef07583ba246a3211ca6b74f
Priyanshu <priyanshu41619@gmail.com>
2025-09-01 22:14:03 +0530

    middlewares
    learning about middlewares and maintaining log.txt

--- BACKEND/server2/server.js
+++ BACKEND/server2/server.js
@@ -1,20 +1,49 @@
-const http  = require("http")
+
 const express = require("express")
-
-
+const fs = require("fs")
 const app = express();
-
-app.get("/" , (req , res) => {
-    return res.end("hello from home page");
-});
-
-app.get("/about" , (req,res)=>{
-    return res.end(`hello ${req.query.name}`)
+const users = require("./MOCK_DATA.json")
+const PORT = 8000
+//middleware
+app.use(express.urlencoded({extended : false}))
+app.use((req , res , next) => {
+fs.appendFile("log.txt" , ` \n ${Date.now()} : ${req.method} : ${req.ip} : ${req.path}` , (err , data) =>{
+    next();
+})
 })
 
+//routes
+ 
+app.get("/" , (req , res) => 
+    {
+    return res.send("hi")})
+app.get("/users" , (req, res)=>{
+   const html =` <ul>
+   ${users.map((users)=> `<li> ${users.first_name}</li>`).join("")}
+    </ul>`;
+    res.send(html); 
+})
+app.get("/api/users" , (req , res) => {
+    return res.json(users)
+})
+app.route("/api/users/:id")
+.get((req, res) => {
+    const id = Number(req.params.id)
+    const user = users.find((user)=> user.id === id)
+    return res.json(user)
+})
+.patch((req,res) => {
+  return res.json({status : "pending"})
+})
+.delete((req,res) => {
+   return res.json({status : "pending"})
+})
+app.post("/api/users" , (req , res) => {
+    const body = req.body
+    users.push({ id : users.length + 1 , ...body })
+    fs.writeFile("./MOCK_DATA.json" , JSON.stringify(users) , (err , data) => {
+        return res.json({status :"success" , id: users.length })
+    })
 
-// const myServer = http.createServer(app);
-
-// myServer.listen(8000 , () => console.log("server started"));|
-
-app.listen(8000 , () => console.log("server started"))
+})
+app.listen(PORT ,  () => console.log(`SERVER STARTED ON ${PORT} PORT`))
